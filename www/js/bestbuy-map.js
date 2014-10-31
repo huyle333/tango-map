d732162dd04e187fd0fe9305b720654339205755
Huy Le <huyle333@yahoo.com>
2014-10-30 21:09:01 -0400

    added some search functionality to find Best Buy products name and price

--- www/js/bestbuy-map.js
+++ www/js/bestbuy-map.js
@@ -1,8 +1,14 @@
 function initialize() {
     var mapOptions = {
-        center: { lat: -34.397, lng: 150.644},
-        zoom: 8
+        center: { lat: 39.8282, lng: -98.5795},
+        zoom: 5,
+        mapTypeControl: false
     };
     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
+
 google.maps.event.addDomListener(window, 'load', initialize);
+
+function myFunction(){
+    alert("hello world");
+}
